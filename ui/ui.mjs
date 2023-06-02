8ff3e7ef0f46f1a2da6da6f9e059cb40a2d81fad
Aureliu555 <A48285@alunos.isel.pt>
2023-06-02 04:07:14 +0100

    home page front-end practically done

--- ui/ui.mjs
+++ ui/ui.mjs
@@ -1,7 +1,11 @@
+import fs from 'node:fs/promises'
+
 export default function uiFunctions(services) {
     
-    function homePage(req, resp) {
-        resp.render("home")
+    async function homePage(req, resp) {
+        const data = await fs.readFile("./professionals.json")
+        const jsonData = JSON.parse(data)
+        resp.render("home", {"professionals": jsonData.professionals})
     }
     
     return {
